4bf64334bdeb784dab8e72c7cce1d4528f8b357b
mariobassem <mariobassem12@gmail.com>
2024-09-01 13:27:13 +0300

    check

--- scripts/githubactions.sh
+++ scripts/githubactions.sh
@@ -1006,10 +1006,10 @@ function hero_upload {
 
 function s3_configure {
 # Check if environment variables are set
-if [ -z "$S3KEYID" ] || [ -z "$S3APPID" ]; then
-    echo "Error: S3KEYID or S3APPID is not set"
-    exit 1
-fi
+# if [ -z "$S3KEYID" ] || [ -z "$S3APPID" ]; then
+#     echo "Error: S3KEYID or S3APPID is not set"
+#     exit 1
+# fi
 
 # Create rclone config file
 mkdir -p "${HOME}/.config/rclone"
